e7e0124733634d77e2fe410dceb10223184810db
Unknown <Stefano-trezzi@virgilio.it>
2018-01-04 16:46:23 +0100

    Aggiornamento regolamento

--- php/regolamento.php
+++ php/regolamento.php
@@ -4,8 +4,28 @@
 	<h4><b><i>1. REGISTRAZIONE E LOGIN</i></b></h4>
 	<p>Per accedere ai servizi è necessario registrarsi al database. Sarà necessario fornire una mail valida, un nickname,
 	un nome per la squadra e una password, che dovrà essere confermata. Una volta effettuata la registrazione è possibile
-  effettuare il login al sito e accedere così ai servizi.</p>
+  effettuare il login al sito e accedere così ai servizi. Una volta concluse le operazioni che si vogliono fare, è bene
+  uscire dall'applicazione effettuando il "Logout", posto in altro a destra della barra di navigazione.</p>
 	<br/>
 	<h4><b><i>2. ACCOUNT</i></b></h4>
-	<p>Nella sezione "Account" è possibile visualizzare i dati del proprio profilo.</p>
+	<p>Nella sezione "Account" è possibile visualizzare e modificare i dati del proprio profilo. Il nickname è unico e
+	non è modificabile. Sempre in questo menu è possibile trovare un sezione per eliminare il proprio account e tutti
+  i dati ad esso correlati nel database. </p>
+	<br/>
+	<h4><b><i>3. SQUADRA</i></b></h4>
+	<p> Una volta registrati al sistema per iscriversi alle giornate di campionato sarà necessario creare una squadra, che dovrà
+	 necessariamente avere 2 portieri, 3 difensori, 3 centrocampisti e 3 attaccanti. Dal menu è possibile visualizzare i propri
+	 dati, compresi i giocatori attualmente in squadra. L'unica informazione modificabile sulla squadra è il motto.<br/>
+   Dalla pagina del fantamercato potremo in ogni momento vendere e comprare giocatori, tenendo sempre d'occhio il budget
+	 rimanente.</p><br/>
+	 <h4><b><i>4. CAMPIONATI</i></b></h4>
+	 <p>Nei campionati anzitutto abbiamo la lista dei campionati. Da qui è possibile osservare i campionati a cui si partecipa
+	 e quelli a cui non si partecipa, ancora in corso. Nelle tabelle mostrate vi è la possibilità di disiscriversi e iscriversi
+	 ai vari campionati.<br/>
+	 La sezione "Crea Campionato" è riservata agli "amministratori" e a coloro che hanno raggiunto il grado di "CT", cioè commissario tecnico. Per diventare
+	 commissari è necessario ottenere almeno 3 stelle. Per ottenerne una serve che in una giornata, una formazione iscritta ottenga 35 punti o più. In questa
+	 pagina si da un nome al campionato che si vuole creare, si scelgono le date tra le quali si giocherà il campionato. Nel menù è inserito ovviamente anche
+	 una pagina dalla quale si potranno eliminare i propri campionati. Cancellandoli tutte le squadre saranno automaticamente rimosse dal campionato, e non
+	 sarà più possibile avere traccia di informazioni ad esso collegate.
+
 </div>
