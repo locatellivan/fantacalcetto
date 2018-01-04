543c048d83995050494b5f7c746fa9890ce5cba4
Unknown <Stefano-trezzi@virgilio.it>
2018-01-04 14:01:49 +0100

    Pagina con Form per creare campionato
    Ivan qua metti alla squadra dell'admin 3 stelle, così vedi se funza.

---
+++ php/creaCampionato.php
@@ -0,0 +1,54 @@
+<?php
+
+	include_once("connessione.php");
+	$nick=$_SESSION['nick'];
+
+	// Salvo in una variabile il nome della squadra dell'utente loggato
+	$sql="SELECT nomeSq FROM squadra JOIN utente ON Mail=Utente WHERE  Nickname='$nick'";
+	$squadra=$cid->query($sql) or die("<p>Impossibile eseguire query.</p>"
+																 ."<p>codice di errore ".$cid->errno
+																 .":".$cid->error."</p>");
+	$nomeSq=$squadra->fetch_row();
+
+	// Salvo in una variabile il numero di stelle della squadra dell'utente loggato
+	$query= "SELECT Stelle FROM squadra WHERE NomeSq='$nomeSq[0]'";
+	$star=$cid->query($query) or die("<p>Impossibile eseguire query.</p>"
+																 ."<p>codice di errore ".$cid->errno
+																 .":".$cid->error."</p>");
+	$stelle=$star->fetch_row();
+
+	if($stelle[0]<3) {
+		echo "<div class='well'>";
+		echo "<h3 align='center'><b>PER CREARE UN CAMPIONATO SONO NECESSARIE 3 O PIU STELLE.</b></h3><br/>";
+		echo "<br/><br/><br/>";
+		echo "<table align='center' border=5><tr><th><h4><b>&nbsp;STELLE OTTENUTE:&nbsp;&nbsp;&nbsp;</b></h4></th><td><h4><b>$stelle[0]</b></h4></td></tr></table></div>";
+	}
+
+	else {
+		echo "<div class='well'>";
+		echo "<h1 align='center'><b>CREA UN CAMPIONATO</b></h1><br/><br/>";
+
+		echo "<form role='form' method='POST' action='php/creaCampionato-exe.php'>
+
+			<div class='form-group'>
+						<label>Nome del Campionato</label>
+						<input class='form-control' type='text' name='nomeCamp' placeholder='Nome Campionato (max. 20 caratteri)'/>
+			</div>
+			<div class='form-group'>
+						<label>Data di Inizio del Campionato</label>
+						<input class='form-control' type='date' name='dataInizio' placeholder='Data Inizio Campionato'/>
+			</div>
+			<div class='form-group'>
+						<label>Data di Fine del Campionato</label>
+						<input class='form-control' type='date' name='dataFine' placeholder='Data Fine Campionato'/>
+			</div>
+
+			<input type='submit' class='btn btn-success' value='Crea Campionato'></input>
+			<input type='reset' class='btn btn-warning' value='Annulla'></input>";
+
+
+		echo "</div>";
+	}
+
+
+ ?>
