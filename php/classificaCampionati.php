d7f250d7c158faeb4ff0ade920b102c495ecd3c0
Unknown <Stefano-trezzi@virgilio.it>
2018-01-06 17:45:04 +0100

    CLASSIFICHE OK

--- php/classificaCampionati.php
+++ php/classificaCampionati.php
@@ -64,6 +64,39 @@
 	<br/><br/>
 	<h2 align="center"><b>CAMPIONATI CONCLUSI</b></h2><br/>
 
-	
+<?php
+
+	$sql="SELECT DISTINCT NomeCamp
+				FROM campionato
+				WHERE CURDATE()>DataFine";
+	$campionatiConc=$cid->query($sql);
+
+	// Se esistono classifiche da mostrare
+	if($campionatiConc->num_rows>=1) {
+			while ($campConc=$campionatiConc->fetch_row()) {
+				echo "<table align='center' border=1>
+							<tr><th colspan='3'><center><h4><b>$campConc[0]</b></h4></center></th></tr>
+							<tr><th><center>Nickname</center></th>
+							<th><center>Nome Squadra</center></th>
+							<th><center>Punti totali</center></th></tr>";
+
+				$sql="SELECT Nickname, NomeSq, PuntiTot FROM utente JOIN squadra on Mail=utente JOIN partecipa on Squadra=NomeSq
+							WHERE Campionato='$campConc[0]'
+							ORDER BY PuntiTot DESC";
+				$utente=$cid->query($sql) or die("<p>Impossibile eseguire query.</p>"
+																				."<p>codice di errore ".$cid->errno
+																				.":".$cid->error."</p>");
+				while($ut=$utente->fetch_row()) {
+						echo "<tr><td><center>$ut[0]</center></td>
+											<td><center>$ut[1]</center></td>
+											<td><center>$ut[2]</center></td></tr>";
+				}
+				echo "</table><br/><br/>";
+			}
+	} else {
+		 echo "<p align='center'>NON C'E' NESSUN CAMPIONATO CONCLUSO.</p>";
+	}
+
+?>
 
 </div>
