5afe98edeaa1e62bf42cea446f92add4382874e9
Unknown <Stefano-trezzi@virgilio.it>
2018-01-07 15:06:10 +0100

    Discirivi dal Campionato => anche dalle formazioni consegnate.

--- php/disiscrivi-exe.php
+++ php/disiscrivi-exe.php
@@ -7,24 +7,23 @@ $nick=$_SESSION['nick'];
 
 $nomeCamp=$_POST['camp'];
 
+// Salvo in una variabile il nome della squadra loggata
 $sql="SELECT nomeSq FROM squadra JOIN utente ON Mail=Utente WHERE Nickname='$nick'";
 $squadra=$cid->query($sql) or die("<p>Impossibile eseguire query.</p>"
 															 ."<p>codice di errore ".$cid->errno
 															 .":".$cid->error."</p>");
-$nomeSq=$squadra->fetch_row();    // Salvo in una variabile il nome della squadra loggata
+$nomeSq=$squadra->fetch_row();
 
 
-// Elimino le tuple in "partecipa" coi campionati selezionati per quella squadra
-
+// Elimino le tuple in "partecipa" e in "iscritta" coi campionati selezionati per quella squadra
 foreach($nomeCamp as $nomiCamp) {
+
 	$query="DELETE FROM partecipa
 	        WHERE Squadra='$nomeSq[0]' AND Campionato='$nomiCamp'";
 	$cid->query($query);
-}
 
-foreach($nomeCamp as $nomiCamp) {
-	$query="DELETE FROM Iscritta
-	        WHERE Formazione=(SELECT IdForm FROM formazione WHERE Squadra.formazione='$nomeSq[0]' ) AND Campionato='$nomiCamp'";
+	$query="DELETE FROM iscritta
+	        WHERE Campionato='$nomiCamp' AND Formazione=(SELECT IdForm FROM formazione WHERE Squadra='$nomeSq[0]')";
 	$cid->query($query);
 }
 
