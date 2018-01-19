62ef7afd6c2418e3ecd0748e584e3e88f9796f2a
Unknown <Stefano-trezzi@virgilio.it>
2018-01-19 16:41:02 +0100

    AGGIUNTA CAMPIONATI FUTURI (funziona tutto, a breve le classifiche)

--- php/listaCampionati.php
+++ php/listaCampionati.php
@@ -44,7 +44,7 @@
 										</tr></form></table><br/><br/>";
 			}
 			else {
-				echo "<p align='center'>NON SI E' ISCRITTI A NESSUN CAMPIONATO</p>";
+				echo "<p align='center'><b>NON SI E' ISCRITTI A NESSUN CAMPIONATO</b></p>";
 			}
 		} else {
 			echo "<p align='center'>ERRORE.</p>";
@@ -57,15 +57,6 @@
 	<br/>
 
 <?php
-	include_once("connessione.php");
-
-	// Salvo in una variabile il nome della squadra dell'utente loggato
-	$sql="SELECT nomeSq FROM squadra JOIN utente ON Mail=Utente WHERE Nickname='".$nick."'";
-	$squadra=$cid->query($sql) or die("<p>Impossibile eseguire query.</p>"
-																 ."<p>codice di errore ".$cid->errno
-																 .":".$cid->error."</p>");
-	$nomeSq=$squadra->fetch_row();
-
 
 	// Query che mostra tutti i CAMPIONATI IN CORSO A CUI NON SI PARTECIPA
 	$query = "SELECT NomeCamp, DataInizio, DataFine, Creatore
@@ -80,31 +71,60 @@
 	$camp= $cid->query($query) or die("<p>Inpossibile eseguire query.</p>"
 																						. "<p>Codice errore " . $cid->errno
 																						. ": " . $cid->error) . "</p>";
-	if($camp) {
-		if($camp->num_rows>=1) {
-			echo "<form role='form' method='POST' action='php/iscriviCamp-exe.php'>
+	if($camp->num_rows>=1) {
+		echo "<form role='form' method='POST' action='php/iscriviCamp-exe.php'>
 												<table align='center' border=1>
 												<tr><th><center>Nome Campionato</center></th><th>Data Inizio</th><th>Data Fine</th><th><center>Creatore</center></th><th><center> ✓</center></th></tr>";
-			while($nomeCamp=$camp->fetch_row()) {
-				echo "<tr><td><center>$nomeCamp[0]</center></td>
-															<td><center>$nomeCamp[1]</center></td>
-															<td><center>$nomeCamp[2]</center></td>
-															<td><center>$nomeCamp[3]</center></td>
-															<td><center><input type='checkbox' name='camp[]' value='".$nomeCamp[0]."'/></center></td></tr>
-															";
+		while($nomeCamp=$camp->fetch_row()) {
+			echo "<tr><td><center>$nomeCamp[0]</center></td>
+						<td><center>$nomeCamp[1]</center></td>
+						<td><center>$nomeCamp[2]</center></td>
+						<td><center>$nomeCamp[3]</center></td>
+						<td><center><input type='checkbox' name='camp[]' value='".$nomeCamp[0]."'/></center></td></tr>";
 			}
 		  echo "<tr><td colspan='5'><center>----&nbsp;&nbsp;&nbsp;<input type='submit' class='btn btn-success' value='ISCRIVITI'></input>&nbsp;&nbsp;&nbsp;----</center></td>
-															</tr></form></table><br/><br/>";
-     } else {
-				echo "<p align='center'>NON C'E' NESSUN CAMPIONATO IN CORSO A CUI NON SEI ISCRITTO.</p>";
-		 }
-	} else {
-		echo "<p align='center'>ERRORE, SQUADRA NON TROVATO.</p>";
+						</tr></form></table><br/><br/>";
+  } else {
+			echo "<p align='center'><b>NON C'E' NESSUN CAMPIONATO IN CORSO A CUI NON SEI ISCRITTO.</b></p>";
 	}
   unset($camp);
+
 ?>
 
-	<h2 align="center"><b>CAMPIONATI CONCLUSI</b></h3><br/>
+	<h2 align="center"><b>CAMPIONATI FUTURI</b></h3><br/>
+
+<?php
+
+	// Query che mostra tutti i CAMPIONATI IN CORSO A CUI NON SI PARTECIPA
+	$query = "SELECT NomeCamp, DataInizio, DataFine, Creatore
+						FROM campionato
+						WHERE CURDATE()<DataInizio
+						ORDER BY DataInizio";
+	$campFut=$cid->query($query) or die("<p>Inpossibile eseguire query.</p>"
+																						. "<p>Codice errore " . $cid->errno
+																						. ": " . $cid->error) . "</p>";
+
+	if($campFut->num_rows>=1) {
+		echo "<table border=1 align='center'>
+					<tr><th><center>Nome Campionato</center></th>
+					<th>Data Inizio</th><th>Data Fine</th>
+					<th><center>Creatore</center></th></tr>";
+			while($caFut=$campFut->fetch_row()) {
+				echo "<tr><td><center>$caFut[0]</center></td>
+							<td><center>$caFut[1]</center></td>
+							<td><center>$caFut[2]</center></td>
+							<td><center>$caFut[3]</center></td></tr>";
+			}
+		echo "</table>";
+	}
+	else {
+		echo "<p align='center'><b>NON C'E' NESSUN CAMPIONATO IN CORSO A CUI NON SEI ISCRITTO.</b></p>";
+	}
+?>
+
+
+
+<!--   <h2 align="center"><b>CAMPIONATI CONCLUSI</b></h3><br/>
 
 <?php
 
@@ -139,5 +159,6 @@
 																			 }
 			}
 		  unset($campConclusi);
- ?>
+ ?> -->
+
 </div>
