5d70c2e4b32875803f0609d93764d07ae44546a7
Unknown <Stefano-trezzi@virgilio.it>
2018-01-24 16:04:41 +0100

    MANCA ALERT "Grazie, i punteggi sono stati aggiornati! Fai download""

--- php/classificheXML.php
+++ php/classificheXML.php
@@ -1,17 +1,23 @@
 <div class="well">
 
-		<h1 align='center'><b>CLASSIFICHE GENERALI</b></h1><br/>
+		<h1 align='center'><b>CLASSIFICHE GENERALI</b></h1>
+		<h5 align='center'><b>Prima di effettuare il download si consiglia di aggiornare il file ai punteggi attuali,
+													potrebbe non essere ancora stato aggiornato.</b></h5><br/>
 
 		<form role="form" method="POST" action="php/generaClasGen-exe.php">
-		<center><input type="submit" class="btn-lg btn-info" value="GENERA CLASSIFICA GENERALE"></input>
+		<center><input type="submit" class="btn-lg btn-info" value="AGGIORNA CLASSIFICA GENERALE"></input>
 		</center></form><br/>
-		<center><a href="XML/clasGenerale.xml" download><button class="btn-lg btn-basic">DOWNLOAD</button></a></center><br/>
+		<center><a href="XML/clasGenerale.xml" download><button class="btn-lg btn-basic">DOWNLOAD FILE</button></a></center><br/>
 
-		<h1 align="center"><b>CLASSIFICA GIORNALIERA</b></h1><br/>
+		<br/><h2 align='center'>______________________</h2><br/>
+
+		<h1 align="center"><b>CLASSIFICA GIORNALIERA</b></h1>
+		<h5 align='center'><b>Prima di effettuare il download si consiglia di aggiornare il file ai punteggi attuali,
+													potrebbe non essere ancora stato aggiornato.</b></h5><br/>
 
 		<form role="form" method="POST" action="php/generaClasGior-exe.php">
-		<center><input type="submit" class="btn-lg btn-info" value="GENERA CLASSIFICA GIORNALIERA"></input>
+		<center><input type="submit" class="btn-lg btn-info" value="AGGIORNA CLASSIFICA GIORNALIERA"></input>
 		</center></form><br/>
-		<center><a href="XML/clasGiornaliera.xml" download><button class="btn-lg btn-basic">DOWNLOAD</button></a></center><br/>
+		<center><a href="XML/clasGiornaliera.xml" download><button class="btn-lg btn-basic">DOWNLOAD FILE</button></a></center><br/>
 
 </div>
