7b324ab2bfd3b0e8743d896ff8c465027ec89cb5
Unknown <Stefano-trezzi@virgilio.it>
2018-01-25 18:03:04 +0100

    REGOLAMENTO FINALE

--- php/regolamento.php
+++ php/regolamento.php
@@ -2,43 +2,65 @@
 	<h1 align="center"><b>REGOLAMENTO</b></h1><br/>
 
 	<h4><b><i>1. REGISTRAZIONE E LOGIN</i></b></h4>
-	<p>Per accedere ai servizi è necessario registrarsi al database. Sarà necessario fornire una mail valida, un nickname,
-	un nome per la squadra e una password, che dovrà essere confermata. Una volta effettuata la registrazione è possibile
-  effettuare il login al sito e accedere così ai servizi. Una volta concluse le operazioni che si vogliono fare, è bene
-  uscire dall'applicazione effettuando il "Logout", posto in altro a destra della barra di navigazione.</p>
+	<p>Per accedere ai servizi è necessario <b>registrarsi</b> al sito. Sarà necessario fornire una mail valida, un nickname,
+	un nome per la squadra e una password, che dovrà essere ripetuta per poterla confermare. Una volta effettuata la registrazione
+	si verrà indirizzati nuovamenta alla HOME, ma da ora sarà possibile effettuare il <b>login</b> al sito e accedere così ai servizi.
+	A destra della barra di navigazione comparirà così il <b>Logout</b>. E' sempre bene uscire dall'applicazione in questo modo.<br/>
+  Con esso compariranno le varie sezioni del sito, in ordine: <i>Account, Squadra, Campionati, Formazioni</i>.</p>
 	<br/>
 	<h4><b><i>2. ACCOUNT</i></b></h4>
-	<p>Nella sezione "Account" è possibile visualizzare e modificare i dati del proprio profilo. Il nickname è unico e
-	non è modificabile. Sempre in questo menu è possibile trovare un sezione per eliminare il proprio account e tutti
-  i dati ad esso correlati nel database. </p>
+	<p>Nella sezione "Account" è possibile <b>visualizzare e modificare i dati del proprio profilo</b>. Come si nota Nickname e Password
+	non sono modificabili. Sempre in questo menu è possibile trovare una sezione per eliminare il proprio account e tutti
+  i dati ad esso correlati nel database.</p>
 	<br/>
 	<h4><b><i>3. SQUADRA</i></b></h4>
-	<p> Una volta registrati al sistema per iscriversi alle giornate di campionato sarà necessario creare una squadra, che dovrà
-	 necessariamente avere 2 portieri, 3 difensori, 3 centrocampisti e 3 attaccanti. Dal menu è possibile visualizzare i propri
-	 dati, compresi i giocatori attualmente in squadra. L'unica informazione modificabile sulla squadra è il motto.<br/>
-   Dalla pagina del fantamercato potremo in ogni momento vendere e comprare giocatori, tenendo sempre d'occhio il budget
-	 rimanente.</p><br/>
+	<p> Per iscriversi alle giornate di campionato sarà necessario creare una squadra, che dovrà
+	 necessariamente avere 2 portieri, 3 difensori, 3 centrocampisti e 3 attaccanti. Dal menu è possibile <b>visualizzare i propri
+	 dati</b>, compresi i giocatori attualmente in squadra. Della propria squadra si può inserire o <b>modificare il motto</b>.<br/>
+   Dalla pagina del <b>fantamercato</b> potremo in ogni momento vendere e comprare giocatori, tenendo sempre d'occhio il budget
+	 rimanente. Questo rappresenta un'anteprima del budget che rimarrebbe con le selezioni effettuate. Ricordati però che una volta
+	 selezionati i giocatori bisogna cliccare il pulsante "COMPRA-VENDI GIOCATORI", posto a fianco del numero dei vostri fantamilioni.
+   Si mostra così una tabella grazie alla quale si possono selezionare i giocatori da vendere, e al di sotto un'altra tabella nella
+   quale si selezionano i giocatori, ordinati in ordine alfabetico per cognome, che si possono comprare. </p><br/>
 	 <h4><b><i>4. CAMPIONATI</i></b></h4>
-	 <p>Nei campionati anzitutto abbiamo la lista dei campionati. Da qui è possibile osservare i campionati a cui si partecipa
-	 e quelli a cui non si partecipa, ancora in corso. Nelle tabelle mostrate vi è la possibilità di disiscriversi e iscriversi
-	 ai vari campionati. Sotto sono mostrati anche i campionati conclusi, con lo storico dei vincitori (se utenti ancora presenti).<br/>
-	 La sezione "Crea Campionato" è riservata agli "amministratori" e a coloro che hanno raggiunto il grado di "CT", cioè commissario tecnico. Per diventare
-	 commissari è necessario ottenere almeno 3 stelle. Per ottenerne una serve che in una giornata, una formazione iscritta ottenga 35 punti o più. In questa
-	 pagina si da un nome al campionato che si vuole creare, si scelgono le date tra le quali si giocherà il campionato. Si fa notare il fatto che quando si
-	 crea un campionato non si viene iscritti automaticamente. Nella sezione per iscriversi comparirà il campionato (se già in corso), e da li ci si potrà
-	 iscrivere. Nel menù è inserito ovviamente anche una pagina dalla quale si potranno eliminare i propri campionati. Cancellandoli tutte le squadre saranno
-	 automaticamente rimosse dal campionato, e non sarà più possibile avere traccia di informazioni ad esso collegate.<br/>
-	 Nella pagina delle classifiche vengono mostrate tutti i punteggi relativi ai campionati, disposti in ordine alfabetico.
+	 <p>Abbiamo anzitutto abbiamo la <b>lista</b> dei campionati. Da qui è possibile osservare i campionati a cui si partecipa
+	 e quelli a cui non si partecipa, ancora in corso. Ad un campionato ci si può iscrivere partendo dalla data di inizio dello stesso.
+	 Iscrivendosi ad un campionato si viene inseriti nella classifica generale dello stesso, con 0 punti. Uscendo invece da un campionato
+	 verranno azzerati i propri punti e perse le informazioni relative alle classifiche per quel campionato.<br/>
+	 La sezione per <b>creare un campionato</b> è disponibile solo a coloro che possiedono il grado di "Amministratore" o "CT", cioè commissario tecnico. Il primo grado
+	 è riservato ai gestori del sito, mentre per diventare commissari è necessario ottenere almeno 3 stelle.
+	 Per ottenere una Stella una formazione deve ottenere 35 o più punti in una giornata di campionato. Ottienine di più partecipando con le 3 formazioni
+	 che si possono consegnare! Nella creazione del campionato gli si da un nome, e si scelgono le date di inizio e di fine. Ricordati che quando si
+	 crea un campionato non si viene iscritti automaticamente, ma lo si dovrà fare nella sezione "Lista". Qui comparirà il campionato, e da li ci si potrà poi
+	 iscrivere (se fatto iniziare alla data ordierna). Come per la creazione è stata inserita una pagina dalla quale si potranno eliminare i propri campionati.
+	 Solo gli amministatori del sito hanno la possibilità di cancellare anche i campionati altrui, specialmente se si utilizzano nomi volgari o blasfemi.
+	 Cancellando un campionato tutte le squadre saranno automaticamente rimosse dal campionato, e non sarà più possibile avere informazioni ad esso collegate.<br/>
+	 Ci sono due differenti sezioni per visualizzare le classifiche. Nella <b>clessifiche generali</b> vengono mostrati i punti totali delle squadre, per tutti i campionati in corso a cui
+   si partecipa. Una volta terminato il campionato sarà possibile visualizzare il relativo campione nello storico (chi ha totalizzato più punti).<br/>
+	 Nelle <b>classifiche giornaliere</b> viene mostrato sopra il numero della giornata per la quale si sono giocate le ultime partite. I voti presi dai tuoi giocatori sono visibili in
+	 una tabella apposita. Sotto di questa vengono mostrati tutti i punteggi giornalieri per tutti i campionati a cui si è iscritto.
+	 Viene poi data la possibilità di <b>esportare le classifiche generali e giornaliere in formato XML</b>. Prima di effettuare il download si consiglia di aggiornare il file,
+	 in caso qualcuno non l'abbia ancora fatto. Cliccando subito su DOWNLOAD si rischia di scaricare il file corrispondente alla giornata precedente.<br/>
+	 Nello <b>storico dei campioni</b> è possibile visualizzare tutti i vincitori dei campionati che si sono conclusi.<br/>
+	 Infine, per <b>campionati fantasma</b> si intendono quei campionati che sono stati giocati, conclusi e vinti da giocatori che si sono eliminati.
    </p><br/>
 	 <h4><b><i>5. FORMAZIONI</i></b></h4>
-	 <p>Qui potremo iscrivere le formazioni alle giornate dei campionati in cui partecipiamo. Una squadra puo avere massimo tre formazioni salvate.
-	 Le formazioni create vengono mostrate. Nel momento in cui si raggiunge il massimo numero di formazioni, fino a che non se ne elimina una non
+	 <p>Qui potremo creare le nostre formazioni e iscriverle alle giornate dei campionati in cui partecipiamo.<br/>
+	 Per la <b>creazione della formazione</b> è richiesto un nome, uno schema e la scelta dei giocatori. Il nome della
+	 formazione verrà seguito automaticamente dal nome della squadra, preceduto da un underscore (_). In base allo schema scelto giocherà
+	 un difensore, un centrocampista o un attaccante in più. E' consentito salvare fino a tre formzioni differenti.
+	 Nel momento in cui si raggiunge il massimo numero di formazioni, fino a che non se ne elimina una non
    viene più data possibilità di crearne altre. Per salvare una formazione sarà necessario avere tutti gli 11 giocatori in squadra.
-	 Oltre a scegliere la posizione di ingresso dei giocatori è necassario scegliere uno schema di gioco tra quelli a disposizione in modo da determinare
-	 i cinque giocatori che partiranno in campo, in caso uno dei giocatori non giochi viene sostituito con il successivo dello stesso ruolo, se nessun giocatore per quel ruolo
-	 gioca quella gionata non si prenderanno punti per quel ruolo.
-   I giocatori in una formazione per prendere punti devono essere iscritti ad una giornata di campionato. Per iscriversi c'è un apposita pagina, dove è necessario
-   scegliere la formazione tra quelle salvate, scegliere un campionato tra quelli a cui si è iscritti e scegliere la gionata per la quale si vuole giocare. Viene tenuta
-   traccia delle formazioni salvate che si sono utilizzate negli ultimi campionati. Per eliminare la formazione basta selezionarla e cliccare il pulsante "ELIMINA FORMAZIONE".
+	 Nella creazione si decidono quindi i cinque giocatori titolari, e in caso un giocatori non giocha la partita (voto uguale a "-1")
+	 viene sostituito con il successivo dello stesso ruolo, se nessun giocatore per quel ruolo gioca quella gionata non si prenderanno punti per quel ruolo.
+   I giocatori in una formazione per prendere punti devono essere iscritti ad una giornata di campionato.<br/>
+	 E' disponibile una pagina dove si <b>consegnano le formazioni</b>,<br/>
+	 ovvero dove si iscrivono alle giornate. Ci si iscrive solo per la giornata mostrata, e si deve scegliere un campionato e una tra le formazioni create. Dopo che si gioca una
+	 giornata le formazioni consegnate restano salvate e iscritte per la prossima giornata dei vari campionati. Se la si vuole modificare o si compra-vendono giocatori al fantamercato
+	 o si sostituisce la formazione con un'altra delle proprie, semplicemente compilando il modulo di consegna con la formazione desiderta. Sotto la pagina sarà presente una tabella con
+	 i dati delle consegne precedenti, se le formazioni non sono cambiate.<br/>
+	 Si possono anche visualizzare le <b>formazioni avversarie</b> Saranno presenti nelle tablle tutti i campionati a cui si partecipa, e per ognuno le formazioni iscritte,
+	 propie e altrui. Vengono mostrati sia i giocatori titolari che le riserve.<br/>
+   Per <b>eliminare le formazioni</b> semplicamente basta selezionarle e cliccare il pulsante "ELIMINA FORMAZIONE".<br/>
    </p><br/>
 </div>
